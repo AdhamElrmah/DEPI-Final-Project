b9619567f8e5df289a99c1621fe7ba0dcce75da6
Adham <adhamelrmahs@gmail.com>
2025-10-13 22:23:14 +0300

    edit trendingcars comp

--- src/components/HomePageComp/TrendingCars.jsx
+++ src/components/HomePageComp/TrendingCars.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Shape from "../../assets/HomePage/TrendingCarsImages/BackgroundRoundShape.svg";
+import ferrari from "../../assets/HomePage/TrendingCarsImages/2019-ferrari-488-gtb.avif";
+import bugatti from "../../assets/HomePage/TrendingCarsImages/2022-bugatti-chiron-sport.avif";
+import lamborghini from "../../assets/HomePage/TrendingCarsImages/2022-lamborghini-aventador.avif";
 
 function TrendingCars({ allCars }) {
   const trendingCars = allCars.filter(
@@ -14,6 +18,16 @@ function TrendingCars({ allCars }) {
       {trendingCars.map((car) => (
         <div key={car.id}>
           <Link to={`/cars/${car.id}`} style={{ color: "red" }}>
+            <img
+              src={
+                car.id === "2019-ferrari-488-gtb"
+                  ? ferrari
+                  : car.id === "2022-bugatti-chiron-sport"
+                  ? bugatti
+                  : lamborghini
+              }
+              alt={car.make}
+            />
             {car.make}
           </Link>
         </div>
